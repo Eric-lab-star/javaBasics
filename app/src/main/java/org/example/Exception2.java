64be95cfe909f7be6c88fbd972b9a124928c1b2b
kimkyungsub <cyon256@icloud.com>
2024-05-27 15:48:45 +0900

    exception2

---
+++ app/src/main/java/org/example/Exception2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Exception2 {
+  public static void run() {
+    int number = 100;
+    int result = 0;
+
+    for (int i = 0; i < 10; i++) {
+      try {
+        result = number / (int) (Math.random() * 10);
+        System.out.println(result);
+      } catch (ArithmeticException e) {
+        System.out.println("0");
+      }
+    }
+  }
+}
