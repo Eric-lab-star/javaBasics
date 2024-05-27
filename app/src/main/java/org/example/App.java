6a8434585ce21940b0b64c2d180153f9dc55d1e2
kimkyungsub <cyon256@icloud.com>
2024-05-27 16:32:34 +0900

    passing exception to outer method using throws

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.io.File;
+
 public class App {
   public static void main(String[] args) {
-    Exception5.run("");
+    File f = null;
+    try {
+      f = Exception5.createFile("happyHacking");
+      System.out.println(f.getName() + " was successfully created");
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
   }
 }
--- app/src/main/java/org/example/Exception5.java
+++ app/src/main/java/org/example/Exception5.java
@@ -3,31 +3,11 @@ package org.example;
 import java.io.File;
 
 public class Exception5 {
-  public static void run(String name) {
-    File f = createFile(name);
-    System.out.println(f.getName());
-  }
-
-  private static File createFile(String filename) {
-    File f = null;
-    try {
-      if (filename == null || filename.equals("")) {
-        throw new Exception("invalid file name");
-      }
-    } catch (Exception e) {
-      filename = "noname.txt";
-    } finally {
-      f = new File(filename);
-      createNewFile(f);
-    }
+  public static File createFile(String filename) throws Exception {
+    if (filename == null || filename.equals(""))
+      throw new IllegalArgumentException("invalid file name" + filename);
+    File f = new File(filename);
+    f.createNewFile();
     return f;
-  }
-
-  private static void createNewFile(File f) {
-    try {
-      f.createNewFile();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 }
